f5bfbee97d66d73d63a3cafd6b4cbd38bdb246b5
Luis Rueda <lurueda@cisco.com>
2017-11-07 16:57:54 -0500

    Adds get_interfaces.py

---
+++ 04-restconf/get_interfaces.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import requests
+import json
+
+PREFIX = "http://"
+HOST = 'ios-xe-mgmt.cisco.com'
+PORT = 9443
+USER = 'root'
+PASS = 'D_Vay!_10&'
+URI = "/restconf/api/running/interfaces"
+
+url = PREFIX + HOST + ":" + str(PORT) + URI
+querystring = {}
+querystring["deep"] = ""
+
+headers = {}
+headers["Authorization"] = "Basic cm9vdDpEX1ZheSFfMTAm"
+headers["Accept"] = "application/vnd.yang.data+json"
+
+response = requests.request("GET", url,
+                            headers=headers,
+                            params=querystring)
+print(json.dumps(response.json(), indent=3))
